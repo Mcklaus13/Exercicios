d68a308d37d57cb0e698b5fb5d4be52d6db58f9d
Victor Mcklaus <84514283+Mcklaus13@users.noreply.github.com>
2021-10-06 10:21:37 -0300

    Delete Aula07a.py

--- Aula07a.py
+++
@@ -1,4 +0,0 @@
-n1 = int(input('Um valor: '))
-n2 = int(input('Outro valor: '))
-print(f'A soma vale {n1 + n2}\nO produto é {n1 * n2} \nA divisão é {n1 / n2:.3f}')
-print(f'Divisão inteira {n1 // n2}\nPotência {n1 ** n2}')
